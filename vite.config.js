bc37282fafeb38a699892dd5142a4bb2ec135c31
Andrés Posada <andres.posada0919@gmail.com>
2022-11-05 18:17:44 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    VitePWA({
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true,
-      },
-    }),
-    react(),
-  ],
+  plugins: [react(), VitePWA()],
 });
